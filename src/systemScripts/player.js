b7dd64085d6f1bdfa16e2fe0fa2ee1840330b32c
BradyBogucki <60437338+BradyBogucki@users.noreply.github.com>
2021-10-18 23:51:36 -0500

    Added player movement rotation and attack

--- src/systemScripts/player.js
+++ src/systemScripts/player.js
@@ -1,11 +1,13 @@
 import Phaser from 'phaser'
+import CONFIG from '../config'
 
 class PlayerClass extends Phaser.Physics.Arcade.Sprite {
   constructor (scene, x, y) {
     super(scene, x, y, 'playerWalkIdle', 0)
 
-    if(!PlayerClass.animInitialize)
-    {
+    this.canMove = true
+
+    if (!PlayerClass.animInitialize) {
       PlayerClass.setupAnim(scene)
     }
 
@@ -16,8 +18,48 @@ class PlayerClass extends Phaser.Physics.Arcade.Sprite {
     this.body.setCollideWorldBounds(true)
 
     this.setScale(0.25, 0.25)
-    this.anims.play('playerWalk')
+    this.anims.play('playerIdle')
+
+    this.on(
+      Phaser.Animations.Events.ANIMATION_COMPLETE_KEY + 'playerAttackPhysical',
+      () => {
+        this.anims.play('playerIdle')
+        this.canMove = true
+        
+      },
+      this)
+    // this.on(
+    //   Phaser.Animations.Events.ANIMATION_COMPLETE_KEY + 'playerAttackMagical',
+    //   () => {
+    //     this.anims.play('playerIdle')
+    //     this.canMove = true
+    //   },
+    //   this)
+
     scene.add.existing(this)
+  }
+
+  attack () {
+    this.canMove = false
+    this.anims.play('playerAttackPhysical', true)
+  }
+
+  magicAttack (x, y) {
+    this.canMove = false
+    console.log('Magic Attack')
+  }
+
+  move (x, y) {
+    if (this.canMove) {
+      if (Math.abs(x) > 0 || Math.abs(y) > 0) {
+        this.anims.play('playerWalk', true)
+      } else {
+        this.anims.play('playerIdle', true)
+      }
+      this.setVelocity(x * CONFIG.WALK_SPEED, y * CONFIG.WALK_SPEED)
+    } else {
+      this.setVelocity(0, 0)
+    }
   }
 }
 
@@ -29,5 +71,20 @@ PlayerClass.setupAnim = (scene) => {
     repeat: -1,
     frames: scene.anims.generateFrameNumbers('playerWalkIdle', { start: 0, end: 7 })
   })
+
+  scene.anims.create({
+    key: 'playerIdle',
+    frameRate: 8,
+    repeat: -1,
+    frames: scene.anims.generateFrameNumbers('playerWalkIdle', { start: 8, end: 11 })
+  })
+
+  scene.anims.create({
+    key: 'playerAttackPhysical',
+    frameRate: 8,
+    repeat: 0,
+    frames: scene.anims.generateFrameNumbers('playerAttack', { start: 0, end: 4 })
+  })
 }
+
 export default PlayerClass
