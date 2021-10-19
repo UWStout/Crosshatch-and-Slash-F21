46508fa967cd54ab000682ea8112a8580d36f637
Kyle Christensen <christensenk1938@my.uwstout.edu>
2021-10-19 13:03:34 -0500

    Updated DataManaging with setters

--- src/scenes/DataManaging.js
+++ src/scenes/DataManaging.js
@@ -1,11 +1,35 @@
 import Phaser from 'phaser'
 
-class DataManaging extends Phaser.Data {
-    create() {
-        this.dataManage = new this.DataManager()
-        this.dataManage.setFreeze(false)
-        this.dataManage.set({name: 'Character', HP: '10', level: '1', STR: '1', INT: '1', CHA: '1'})
-    }
+class DataManaging extends Phaser.Data.DataManager {
+  create () {
+    this.dataManage = new this.DataManager()
+    this.dataManage.setFreeze(false)
+    this.dataManage.set({ name: 'Character', HP: '10', level: '1', STR: '1', INT: '1', CHA: '1', Gold: '0' })
+  }
+
+  setHp () {
+    this.dataManage.HP++
+  }
+
+  setStr () {
+    this.dataManage.STR++
+  }
+
+  setInt () {
+    this.dataManage.INT++
+  }
+
+  setChar () {
+    this.dataManage.CHA++
+  }
+
+  setGold (collectedGold) {
+    this.dataManage.Gold += collectedGold
+  }
+
+  setLevel () {
+    this.dataManage.level++
+  }
 }
 
-export default DataManaging;
+export default DataManaging
