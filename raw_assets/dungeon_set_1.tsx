e6053ff3009f4f24d0bcace89b69e7b450a63d60
Kyle <christensenk1938@my.uwstout.edu>
2021-10-21 06:38:09 -0500

    Added tsx file for Tutorial Dungeon

---
+++ raw_assets/dungeon_set_1.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tile_dungeon1" tilewidth="288" tileheight="288" tilecount="6" columns="6">
+ <image source="tile_wall_help2.png" width="1728" height="288"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="288" height="288"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="102" width="288" height="84"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="102" width="237" height="84"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="0" y="102" width="186" height="84"/>
+   <object id="4" x="102" y="102" width="84" height="186"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="102" y="102" width="84" height="186"/>
+   <object id="2" x="0" y="102" width="186" height="84"/>
+  </objectgroup>
+ </tile>
+</tileset>
