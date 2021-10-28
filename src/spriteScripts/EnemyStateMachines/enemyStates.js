0d968363da06b1bab1d434af41957f97aee39a5d
BradyBogucki <60437338+BradyBogucki@users.noreply.github.com>
2021-10-28 10:57:16 -0500

    EnemyStates

---
+++ src/spriteScripts/EnemyStateMachines/enemyStates.js
@@ -0,0 +1,9 @@
+const EnemyStates = Object.freeze({
+  GUARDING: 'GUARDING',
+  PURSUING: 'PURSUING',
+  ENGAGING: 'ENGAGING',
+  RECOVERING: 'RECOVERING',
+  DYING: 'DYING',
+  UNKNOWN: 'UNKNOWN'
+})
+export default EnemyStates
