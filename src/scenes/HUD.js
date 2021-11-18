f7386deabf9fe9a644e67f604f27c30c52e5313d
Kyle Christensen <christensenk1938@my.uwstout.edu>
2021-11-18 12:47:37 -0600

    Font work (not working) and HUD loading

--- src/scenes/HUD.js
+++ src/scenes/HUD.js
@@ -3,13 +3,34 @@ import Phaser from 'phaser'
 import CONFIG from '../config.js'
 
 class HUDScene extends Phaser.Scene {
+  constructor () {
+    super()
+  }
+
   create () {
-    this.loadingText = this.add.text(
-      CONFIG.DEFAULT_WIDTH - 10,
-      CONFIG.DEFAULT_HEIGHT - 10,
-      'INFORMATION', { font: '16pt Arial', color: '#FF0000', align: 'center' }
+    const ui = this.add.image(970, 960, 'uiOutline')
+    ui.setScale(0.844, 0.844)
+    const element = document.createElement('style')
+    document.head.appendChild(element)
+    const sheet = element.sheet
+    const styles = '@font-face { font-family: "Versal Filled"; src: url("assets/VersalFilled-y1r3.ttf") format("opentype"); }'
+    sheet.insertRule(styles, 0)
+    
+    this.loadingText = this.add.bitmapText(
+      CONFIG.DEFAULT_WIDTH / 2 + 50,
+      CONFIG.DEFAULT_HEIGHT - 80,
+      'Versal', { fontFamily: 'versal', color: '#000000', align: 'center' }
     )
+    // Phaser.Display.Align.In.Center(ui, this.add.zone(700, 940, 300, 300))
     this.loadingText.setOrigin(1, 1)
+  }
+
+  updateHealth (newHealth) {
+
+  }
+
+  updateExp (newExp) {
+
   }
 }
 
