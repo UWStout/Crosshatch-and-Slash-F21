cd0c9cdbcca62b92e222877b12c780b3434311da
Kyle <christensenk1938@my.uwstout.edu>
2021-12-12 13:08:33 -0600

    Game Over Screen

--- src/scenes/Example.js
+++ src/scenes/Example.js
@@ -355,11 +355,11 @@ class ExampleScene extends Phaser.Scene {
       console.log(this.player.x, this.player.y)
     }
 
-    // if (this.player.getHealth() <= 0) {
-    //   this.scene.stop()
-    //   this.scene.stop('HUDScene')
-    //   this.scene.start('GameOverScene')
-    // }
+    if (this.player.getHealth() <= 0) {
+      this.scene.stop('HUDScene')
+      this.scene.stop()
+      this.scene.start('GameOverScene')
+    }
 
     // if (this.cursors.open.isDown) {
     //   if (this.chestArray) {
--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -2,29 +2,86 @@ import Phaser from 'phaser'
 import CONFIG from '../config'
 
 class GameOverScene extends Phaser.Scene {
+  preload () {
+    this.load.image('restartButton', 'assets/sprites/buttons/spr_UI_buttonRestart.png')
+  }
+
   create () {
-    this.add.image(CONFIG.DEFAULT_WIDTH / 2, CONFIG.DEFAULT_HEIGHT / 2, 'bones')
+    this.add.image(CONFIG.DEFAULT_WIDTH / 2 + 100, CONFIG.DEFAULT_HEIGHT / 2, 'bones')
     const text1 = this.add.text(
       CONFIG.DEFAULT_WIDTH / 2,
       100,
-      'You have fallen.', { fontFamily: 'hamlet', color: '#000000', fontSize: 45 }
+      'You have fallen.', { fontFamily: 'hamlet', color: '#FFFFFF', fontSize: 45 }
     )
-    text1.setAlpha(0)
+    text1.alpha = 0
+
     const text2 = this.add.text(
-      CONFIG.DEFAULT_WIDTH / 2,
+      CONFIG.DEFAULT_WIDTH / 2 - 150,
       300,
-      'The cycle will continue forever more.', { fontFamily: 'hamlet', color: '#000000', fontSize: 45 }
+      'The cycle will continue forever more.', { fontFamily: 'hamlet', color: '#FFFFFF', fontSize: 45 }
     )
-    text2.setAlpha(0)
+    text2.alpha = 0
+
     const text3 = this.add.text(
       CONFIG.DEFAULT_WIDTH / 2,
       500,
-      'GAME OVER', { fontFamily: 'hamlet', color: '#000000', fontSize: 45 }
+      'GAME OVER', { fontFamily: 'hamlet', color: '#FFFFFF', fontSize: 45 }
     )
-    text3.setAlpha(0)
-    const text1tween = this.tweens.add({
+    text3.alpha = 0
+
+    const restartButton = this.add.image(CONFIG.DEFAULT_WIDTH / 2 - 400, CONFIG.DEFAULT_HEIGHT / 2, 'restartButton')
+    restartButton.setInteractive()
+    restartButton.on('pointerdown', () => {
+      this.scene.stop()
+      this.scene.start('ExampleScene')
+    })
+    restartButton.alpha = 0
+
+    const exitButton = this.add.image(CONFIG.DEFAULT_WIDTH / 2 + 600, CONFIG.DEFAULT_HEIGHT / 2, 'exitButton')
+    exitButton.setInteractive()
+    exitButton.on('pointerdown', () => {
+      this.scene.stop()
+      this.scene.start('StartScene')
+    })
+    exitButton.alpha = 0
+
+    this.tweens.add({
       targets: text1,
-      duration: 2000
+      duration: 2000,
+      alpha: 1,
+      ease: 'Power1'
+    })
+
+    this.tweens.add({
+      targets: text2,
+      duration: 2000,
+      alpha: 1,
+      ease: 'Power1',
+      delay: 2500
+    })
+
+    this.tweens.add({
+      targets: text3,
+      duration: 2000,
+      alpha: 1,
+      ease: 'Power1',
+      delay: 5000
+    })
+
+    this.tweens.add({
+      targets: exitButton,
+      duration: 2000,
+      alpha: 1,
+      ease: 'Power1',
+      delay: 7500
+    })
+
+    this.tweens.add({
+      targets: restartButton,
+      duration: 2000,
+      alpha: 1,
+      ease: 'Power1',
+      delay: 7500
     })
   }
 }
